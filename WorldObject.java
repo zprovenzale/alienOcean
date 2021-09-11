7cdde640520ca903f6210508bb81a3981dead749
zprovenzale <zprovenzale@smith.edu>
2021-09-11 14:46:42 -0400

    Added WorldObject xyz vars, getters, and setters

--- WorldObject.java
+++ WorldObject.java
@@ -1,3 +1,33 @@
 public abstract class WorldObject {
+    int x;
+    int y;
+    int z;
+
+    //GETTERS
+
+    public static void getX() {
+        return this.x;
+    }
+
+    public static void getY() {
+        return this.y;
+    }
+
+    public static void getZ() {
+        return this.z;
+    }
     
+    //SETTERS
+
+    public static void setX(int newX) {
+        this.x = newX;
+    }
+
+    public static void setY(int newY) {
+        this.y = newY;
+    }
+
+    public static void setZ(int newZ) {
+        this.z = newZ;
+    }
 }
